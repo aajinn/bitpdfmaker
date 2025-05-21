56c8d6568a17ba160018bb1082f84a7d24ef44a5
ARed99 <chandyajin@gmail.com>
2025-05-21 20:39:14 +0530

    footer changes

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -12,12 +12,9 @@ export default function RootLayout({
                <main className="flex-1 container mx-auto ml-3 text-2xl break-words ">
                     {children}
                </main>
-               <footer className="bg-gray-100 border-t">
-                    <div className="container mx-auto py-8 text-center text-gray-500 text-sm">
-                         {/* Footer content */}
-                         &copy; {new Date().getFullYear()} BitMakerPdf. All
-                         rights reserved.
-                    </div>
+               <footer className="text-bg-indigo-400 py-6 text-center select-none">
+                    &copy; {new Date().getFullYear()} BitMakerPdf. All rights
+                    reserved.
                </footer>
           </div>
      );
