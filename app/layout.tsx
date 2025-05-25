f1814f57008ffb2ddf8f1e9921c1e7c20c178be6
ARed99 <chandyajin@gmail.com>
2025-05-25 19:08:43 +0530

    import external script component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import ExternalScripts from "./components/ExternalScripts";
+
 export const metadata = {
      title: "BitPDFMaker - PDF Note Extractor & Cheat Sheet Generator",
      description:
@@ -12,7 +14,10 @@ export default function RootLayout({
 }>) {
      return (
           <html lang="en">
-               <body>{children}</body>
+               <body>
+                    <ExternalScripts />
+                    {children}
+               </body>
           </html>
      );
 }
