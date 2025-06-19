971f183686b1bd5c539d682df5a4debad3642acf
ARed99 <chandyajin@gmail.com>
2025-06-19 21:03:50 +0530

    rename tool path names to keep standard

--- app/(tools)/merge-pdf/page.tsx
+++ app/(tools)/merge-pdf/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { useState, useRef, DragEvent, useCallback, memo } from "react";
 import Header from "../../components/Header";
 import ExternalScripts from "../../components/ExternalScripts";
